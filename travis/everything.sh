ff82ec57576161ac282b6896452de2f6af361b72
Akshobhya K M <akshobhyakm@gmail.com>
2022-01-02 12:24:48 -0500

    Update everything.sh

--- travis/everything.sh
+++ travis/everything.sh
@@ -3,5 +3,5 @@
 find src/ -name '[^\.]*.agda' \
     | sed -e 's|^src/[/]*|import |' -e 's|/|.|g' -e 's/.agda//' -e '/import Everything/d' \
     | LC_COLLATE='C' sort \
-                > Everything.agda
+                > Everything.agda 
 cat Everything.agda >> index.agda
