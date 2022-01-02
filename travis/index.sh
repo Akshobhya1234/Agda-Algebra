68bd5887513db90503732d37168c489ee81172a0
Akshobhya K M <akshobhyakm@gmail.com>
2022-01-02 12:29:42 -0500

    Update index.sh

--- travis/index.sh
+++ travis/index.sh
@@ -3,6 +3,6 @@ set -o pipefail
 for file in $( find src -name "*.agda" | sort ); do
   if [[ ! $( head -n 10 $file | grep -m 1 "This module is DEPRECATED" ) ]]; then
     i=$( echo $file | sed 's/src\/\(.*\)\.agda/\1/' | sed 's/\//\./g' )
-    echo "import $i" >> index.agda;
+    echo "import $i" >> index.agda; 
   fi
 done
